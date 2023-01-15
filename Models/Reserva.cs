6abcf37c74a41f6baeae4c8063a2270080245404
Otavio Senra <araujofletes@gmail.com>
2023-01-15 17:48:39 -0300

    Commit

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -13,48 +13,28 @@ namespace DesafioProjetoHospedagem.Models
             DiasReservados = diasReservados;
         }
 
-        public void CadastrarHospedes(List<Pessoa> hospedes)
-        {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
-            {
+        public void CadastrarHospedes(List<Pessoa> hospedes){
+            Console.WriteLine($"Capacidade da Suite: {Suite.Capacidade} Número de hospedes: {hospedes.Count()}");
+            if (Suite.Capacidade >= hospedes.Count()){
                 Hospedes = hospedes;
             }
-            else
-            {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+            else{
+                Console.WriteLine("Não foi possível realizar cadastro: Capacidade ultrapassada");
             }
         }
 
-        public void CadastrarSuite(Suite suite)
-        {
+        public void CadastrarSuite(Suite suite){
             Suite = suite;
         }
 
-        public int ObterQuantidadeHospedes()
-        {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+        public int ObterQuantidadeHospedes(){
+            return Hospedes.Count();
         }
 
-        public decimal CalcularValorDiaria()
-        {
-            // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+        public decimal CalcularValorDiaria(){
+            decimal valor = DiasReservados * Suite.ValorDiaria;
 
-            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
-            {
-                valor = 0;
-            }
-
-            return valor;
+            return DiasReservados >= 10 ? (valor/100 * 90) : valor;
         }
     }
 }
